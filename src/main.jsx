de47e60134cf222260b1173bd1ace1847cbb521f
zahirraihan26 <mdzahirraihanbakul@gmail.com>
2026-03-04 01:18:13 +0600

    Add GA4 tracking

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import NavigateProvider from './context/NavigateProvider';
 import ReactGA from 'react-ga4';
 
 // ✅ Initialize your Google Analytics
-ReactGA.initialize("G-9QH68429Q0"); // <-- তোমার Measurement ID বসাও
+ReactGA.initialize("G-LQ5B5SC864"); // <-- তোমার GA4 ID
 ReactGA.send("pageview"); // <-- Home page visitor count
 
 createRoot(document.getElementById('root')).render(
